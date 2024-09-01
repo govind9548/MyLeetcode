5a8044bb14103b329ca58eb923e2e6c6b4e8cdeb
Govind <167590582+govind9548@users.noreply.github.com>
2024-09-02 00:15:30 +0530

    Create Convert_1D_Array_Into_2D_Array_2022.java

---
+++ Convert_1D_Array_Into_2D_Array_2022.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int len = original.length;
+        int arr[][] = new int[m][n];
+
+        if(len!=m*n){
+            return new int[0][0];
+        }
+        
+        int x=0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j] = original[x];
+                x++;
+            }
+        }
+        return arr;
+    }
+}
